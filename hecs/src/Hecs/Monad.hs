08195c637c942980b0150d696b2acfa62e5af7e9
Jannis <overesch.jannis@gmail.com>
2023-01-14 23:08:11 +0100

    Change HecsM to use an IORef instead of being a state monad

--- hecs/src/Hecs/Monad.hs
+++ hecs/src/Hecs/Monad.hs
@@ -8,46 +8,54 @@ module Hecs.Monad (
 import Hecs.Monad.Class
 import qualified Hecs.World as Core
 
-import Control.Monad.Trans.State.Strict
+import Control.Monad.Trans.Reader
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Control
 import Control.Monad.Base
 import Hecs.Filter
+import Data.IORef
 
-newtype HecsM w m a = HecsM { unHecsM :: StateT w m a }
+newtype HecsM w m a = HecsM { unHecsM :: ReaderT (IORef w) m a }
   deriving newtype (Functor, Applicative, Monad, MonadIO, MonadBase b, MonadBaseControl b)  
 
-runHecsM :: Monad m => w -> HecsM w m a -> m a
-runHecsM w (HecsM stM) = evalStateT stM w
+runHecsM :: MonadIO m => w -> HecsM w m a -> m a
+runHecsM w (HecsM f) = liftIO (newIORef w) >>= runReaderT f 
 {-# INLINE runHecsM #-}
 
 -- TODO Relax MonadIO to PrimMonad
 
 instance (MonadBaseControl IO m, Core.WorldClass w) => MonadHecs w (HecsM w m) where
   newEntity = HecsM $ do
-    w <- get
+    wRef <- ask
+    w <- liftBase $ readIORef wRef
     (w', eid) <- liftBase $ Core.allocateEntity w
-    put w'
+    liftBase $ writeIORef wRef w'
     pure eid
   {-# INLINE newEntity #-}
-  freeEntity eid = HecsM $ get >>= liftBase . flip Core.deAllocateEntity eid >>= put
+  freeEntity eid = HecsM $ do
+    wRef <- ask
+    w <- liftBase $ readIORef wRef 
+    w' <- liftBase $ Core.deAllocateEntity w eid
+    liftBase $ writeIORef wRef w'
   {-# INLINE freeEntity #-}
   setComponentWithId eid compId comp = HecsM $ do
-    w <- get
+    wRef <- ask
+    w <- liftBase $ readIORef wRef
     w' <- liftBase $ Core.setComponentWithId w eid compId comp
-    put w'
+    liftBase $ writeIORef wRef w'
   {-# INLINE setComponentWithId #-}
   getComponentWithId eid compId s f = HecsM $ do
-    w <- get
+    w <- ask >>= liftBase . readIORef
     mC <- liftBase $ Core.getComponentWithId w eid compId (pure . Just) (pure Nothing)
     case mC of
       Nothing -> unHecsM f
       Just c -> unHecsM $ s c
   {-# INLINE getComponentWithId #-}
   filter :: forall b ty . Filter ty HasMainId -> (TypedArchetype ty -> b -> HecsM w m b) -> HecsM w m b -> HecsM w m b
-  filter fi f z = HecsM get >>= \w -> do
+  filter fi f z = HecsM ask >>= \wRef -> do
     st <- liftBaseWith $ \runInBase -> do
-      Core.filter @_ @_ @(StM m (b, w)) w fi
+      w <- readIORef wRef
+      Core.filter @_ @_ @(StM m b) w fi
         (\aty acc -> runInBase $ restoreM acc >>= f aty)
         (runInBase z)
     restoreM st
