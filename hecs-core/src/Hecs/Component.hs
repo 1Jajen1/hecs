653a25812d804863d172c224df55c5fa62b51c76
Jannis <overesch.jannis@gmail.com>
2023-01-15 02:31:12 +0100

    Add ViaBoxed and tag

--- hecs-core/src/Hecs/Component.hs
+++ hecs-core/src/Hecs/Component.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeFamilies #-}
 module Hecs.Component (
   ComponentId(..)
 , Component(Backend)
@@ -6,7 +7,21 @@ module Hecs.Component (
 , StorableBackend
 , ArrayBackend
 , IterateBackend(..)
+, ViaBoxed(..)
+, tag
 ) where
 
 import Hecs.Component.Internal
 import Hecs.Component.Generic
+import Data.Kind
+
+tag :: forall (a :: Type) . a
+tag = undefined
+
+newtype ViaBoxed a = ViaBoxed a
+
+instance Component (ViaBoxed a) where
+  type Backend (ViaBoxed a) = ArrayBackend a
+  type (Store (ViaBoxed a)) = a
+  backing _ b _ = b
+  {-# INLINE backing #-}
